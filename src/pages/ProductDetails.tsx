66aa391fc4a0a7d3162432310afd942fde25a08b
GearHead87 <hosanulislam87@gmail.com>
2024-07-02 16:23:27 +0600

    product details card added on details page

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -1,16 +1,39 @@
-import { useParams } from "react-router-dom";
-import { useGetProductDetailsQuery } from "../redux/api/baseApi";
+import { useParams } from 'react-router-dom';
+import { useGetProductDetailsQuery } from '../redux/api/baseApi';
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+import { Avatar, Card } from 'antd';
+
+const { Meta } = Card;
 
 const ProductDetails = () => {
-    const {id} = useParams()
-    console.log(id);
-    const {data, isLoading, isError} = useGetProductDetailsQuery(id)
-    console.log(data, isError, isLoading);
-    return (
-        <div>
-            <h2>this is product</h2>
-        </div>
-    );
+	const { id } = useParams();
+	console.log(id);
+	const { data, isLoading, isError } = useGetProductDetailsQuery(id);
+	console.log(data);
+	return (
+		<div>
+			<Card
+                className='max-w-lg mx-auto'
+				cover={
+					<img
+						alt="example"
+						src={data.images[0]}
+					/>
+				}
+				actions={[
+					<SettingOutlined key="setting" />,
+					<EditOutlined key="edit" />,
+					<EllipsisOutlined key="ellipsis" />,
+				]}
+			>
+				<Meta
+					avatar={<Avatar src={data.images[0]} />}
+					title={data.title}
+					description={data.description}
+				/>
+			</Card>
+		</div>
+	);
 };
 
 export default ProductDetails;
