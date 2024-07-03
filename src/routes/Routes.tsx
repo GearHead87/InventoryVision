27c2a0e696f8f75a683cb0c6843528ad2aa22fcc
GearHead87 <hosanulislam87@gmail.com>
2024-07-03 13:55:19 +0600

    error page added and netlify 404 bug fix

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from 'react-router-dom';
 import MainLayout from '../layouts/MainLayout';
 import Home from '../pages/Home';
 import ProductDetails from '../pages/ProductDetails';
+import Error from '../pages/Error';
 
 export const router = createBrowserRouter([
 	{
 		path: '/',
 		element: <MainLayout />,
+		errorElement: <Error />,
 		children: [
 			{
 				path: '/',
